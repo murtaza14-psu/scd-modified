987456255e9af52a533bf667b068046f4c187103
S.Hussain <syedmhussain209@gmail.com>
2025-03-17 13:39:02 +0500

    add profile pic and change project name

--- VolunteerConnect/VolunteerConnect/urls.py
+++ VolunteerConnect/VolunteerConnect/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for test1 project.
+URL configuration for VolunteerConnect project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
